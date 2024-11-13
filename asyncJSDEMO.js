e806a7dd6fa6d33f6d5dd1023c546c98eed69d46
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-14 00:02:35 +0530

    added Asynchronous Programming

--- asyncJSDEMO.js
+++ asyncJSDEMO.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+//callback example
+function disp(n) {
+    n();
+}
+disp(function () {
+    console.log("Hello");
+});
+
+//Aynchronous programming
+console.log("Before setTimeout");
+setTimeout(function () {
+    console.log("Inside setTimeout");
+}, 2000);
+console.log("After setTimeout");
+
