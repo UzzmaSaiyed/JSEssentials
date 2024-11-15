5b1fda0cb14d7fb7e37c35275f1dce543cddef41
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-15 12:51:20 +0530

    updated

--- asyncJSDEMO.js
+++ asyncJSDEMO.js
@@ -7,9 +7,27 @@ disp(function () {
 });
 
 //Aynchronous programming
+
+//setTimeout
 console.log("Before setTimeout");
 setTimeout(function () {
     console.log("Inside setTimeout");
-}, 2000);
+}, 1000);
 console.log("After setTimeout");
+
+
+// setInterval
+console.log("Before setInterval");
+const intervalID = setInterval(function () {
+    console.log("Inside setInterval");
+}, 1000);
+console.log("After setInterval");
+setTimeout(function () {
+    clearInterval(intervalID);
+    console.log("Interval cleared");
+}, 5000);
+
+
+
+
 
