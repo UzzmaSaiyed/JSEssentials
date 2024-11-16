96abae157d4d593d7e212ac6cc6bc08bfaf8615e
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-16 18:44:46 +0530

    added Promise

--- asyncJSDEMO.js
+++ asyncJSDEMO.js
@@ -25,8 +25,36 @@ console.log("After setInterval");
 setTimeout(function () {
     clearInterval(intervalID);
     console.log("Interval cleared");
-}, 5000);
+}, 3000);
 
+//Complex Callback example
+function getData(cb) {
+    let users = [{ name: 'John', age: 25 }, { name: 'Joe', age: 28 }];
+    cb(users);
+}
+function getHobby(users, cb) {
+    let hobby = [{ hobby: "Reading" }, { hobby: "Singing" }];
+    cb(hobby);
+    console.log(users.name);
+}
+
+setTimeout(function () {
+    getData(function (users) {
+        setTimeout(function () {
+            getHobby(users[1], function (hobby) {
+                console.log(hobby[1]);
+            })
+        }, 4000);
+
+    });
+}, 6000);
+
+//Promise
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Hello from Promise");
+    }, 8000);
+}).then(result => console.log(result));
 
 
 
